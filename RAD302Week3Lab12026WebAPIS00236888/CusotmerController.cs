54407f97f356db800a41066fbf34346aabff74da
ryanmc1407 <ryanmcclelland@msn.com>
2026-02-05 11:59:54 +0000

    Step 30 done

---
+++ RAD302Week3Lab12026WebAPIS00236888/CusotmerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RAD302Week3Lab12026WebAPIS00236888
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CusotmerController : ControllerBase
+    {
+    }
+}
